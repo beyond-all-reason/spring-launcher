617f86daaeb29cca33ad2f699b0b5936fe95778b
Marek Rusinowski <marekrusinowski@gmail.com>
2024-02-20 22:23:15 +0100

    Fix eslint linting errors

--- src/exts/discord_integration.js
+++ src/exts/discord_integration.js
@@ -1,11 +1,11 @@
 const { bridge } = require('../spring_api');
 const { log } = require('../spring_log');
 const { config } = require('../launcher_config');
-const DiscordRPC = require("discord-rpc");
+const DiscordRPC = require('discord-rpc');
 
 if (!config.discord_rich_presence?.application_id) {
-    log.warn("config.discord_rich_presence.application_id not defined, integration disabled");
-    return
+	log.warn('config.discord_rich_presence.application_id not defined, integration disabled');
+	return;
 }
 
 // Application ID from https://discord.com/developers/applications/<applicationId>/
@@ -15,68 +15,68 @@ let rpc = null;
 DiscordRPC.register(applicationId);
 
 async function TryToLogin() {
-    if (rpc) return;
-    try {
-        rpc = new DiscordRPC.Client({ transport: 'ipc' });
-        rpc.on('disconnected', () => {
-            rpc = null;
-        });
-        await rpc.login({ clientId: applicationId });
-    } catch (error) {
-        rpc = null;
-        log.warn("Discord RPC login error - Discord not running?");
-    }
+	if (rpc) return;
+	try {
+		rpc = new DiscordRPC.Client({ transport: 'ipc' });
+		rpc.on('disconnected', () => {
+			rpc = null;
+		});
+		await rpc.login({ clientId: applicationId });
+	} catch (error) {
+		rpc = null;
+		log.warn('Discord RPC login error - Discord not running?');
+	}
 }
 
 bridge.on('DiscordSetActivity', async command => {
-    // command
-    // https://discord.com/developers/docs/rich-presence/how-to#updating-presence-update-presence-payload-fields
-    // {
-    //     state : string - Playing, spectating, in menu, in lobby, watching replay...
-    //     details : string - Map name
-    //     startTimestamp : unix timestamp - including will show time as "elapsed"
-    //     playerCount : int > 0
-    //     maxPlayerCount : int > 0
-    //     partyId : string - battle ID for now
-    //     largeImageText : string - tooltip for the largeImageKey
-    //     smallImageKey : string - name of the uploaded image for the large profile artwork, can also be URL
-    //     smallImageText : string - tooltip for the smallImageKey
-    // }
-    if (!rpc) await TryToLogin()
-    if (!rpc) return
+	// command
+	// https://discord.com/developers/docs/rich-presence/how-to#updating-presence-update-presence-payload-fields
+	// {
+	//     state : string - Playing, spectating, in menu, in lobby, watching replay...
+	//     details : string - Map name
+	//     startTimestamp : unix timestamp - including will show time as "elapsed"
+	//     playerCount : int > 0
+	//     maxPlayerCount : int > 0
+	//     partyId : string - battle ID for now
+	//     largeImageText : string - tooltip for the largeImageKey
+	//     smallImageKey : string - name of the uploaded image for the large profile artwork, can also be URL
+	//     smallImageText : string - tooltip for the smallImageKey
+	// }
+	if (!rpc) await TryToLogin();
+	if (!rpc) return;
 
-    let activity = {
-        state: command.state,
-        details: command.mapName,
-        startTimestamp: command.startTimestamp,
-        largeImageText: command.largeImageText ?? config.discord_rich_presence.large_image_text_default,
-        smallImageKey: command.smallImageKey ?? config.discord_rich_presence.small_image_key_default,    
-        smallImageText: command.smallImageText ?? config.discord_rich_presence.small_image_text_default,
-        buttons: [{
-            label: config.discord_rich_presence.button_label,
-            url: config.discord_rich_presence.button_url
-        }]
-    };
+	let activity = {
+		state: command.state,
+		details: command.mapName,
+		startTimestamp: command.startTimestamp,
+		largeImageText: command.largeImageText ?? config.discord_rich_presence.large_image_text_default,
+		smallImageKey: command.smallImageKey ?? config.discord_rich_presence.small_image_key_default,    
+		smallImageText: command.smallImageText ?? config.discord_rich_presence.small_image_text_default,
+		buttons: [{
+			label: config.discord_rich_presence.button_label,
+			url: config.discord_rich_presence.button_url
+		}]
+	};
 
-    if (command.mapName && config.discord_rich_presence.minimap_url) {
-        activity.largeImageKey = config.discord_rich_presence.minimap_url.replace("<map>", encodeURIComponent(command.mapName));
-    } else {
-        activity.largeImageKey = config.discord_rich_presence.large_image_key_default;
-    }
+	if (command.mapName && config.discord_rich_presence.minimap_url) {
+		activity.largeImageKey = config.discord_rich_presence.minimap_url.replace('<map>', encodeURIComponent(command.mapName));
+	} else {
+		activity.largeImageKey = config.discord_rich_presence.large_image_key_default;
+	}
 
-    // Both playerCount and maxPlayerCount have to be > 0 to avoid errors
-    if (command.playerCount > 0 && command.maxPlayerCount > 0) {
-        activity.partySize = command.playerCount;
-        activity.partyMax = command.maxPlayerCount;
-    }
+	// Both playerCount and maxPlayerCount have to be > 0 to avoid errors
+	if (command.playerCount > 0 && command.maxPlayerCount > 0) {
+		activity.partySize = command.playerCount;
+		activity.partyMax = command.maxPlayerCount;
+	}
 
-    if (command.partyId) {
-        activity.partyId = command.battleId.toString();
-    }
+	if (command.partyId) {
+		activity.partyId = command.battleId.toString();
+	}
 
-    try {
-        await rpc.setActivity(activity);
-    } catch (error) {
-        log.warn("Error while setting activity:", error)
-    }
+	try {
+		await rpc.setActivity(activity);
+	} catch (error) {
+		log.warn('Error while setting activity:', error);
+	}
 });
